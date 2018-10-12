4d9675daa4cfbb0795fdb17289eb96582e1fbc02
Kieran Coughlan <KieranCoughlan@gmail.com>
2018-10-13 00:53:32 +0100

    Small tweaks

--- gameController.js
+++ gameController.js
@@ -1,22 +1,29 @@
 let currentLevel; // A variable for the current level
 let levels = [];  // A list for all levels;
 let player;       // A variable for the player
-let input;       // A variable for input processing
+let input;        // A variable for input processing
 let fps = 30;     // Frame rate
 
 function setup() {
     createCanvas(500, 300);
     frameRate(fps);
     
-    levels.push(new Level());
+    setupLevels();
     currentLevel = levels[0];
    
     let playerSize = 20;
     let playerPos = createVector(playerSize / 2, 
                                  height - playerSize / 2);
 
-    player = new PlayerController(playerPos, playerSize, 1, 5, 1 / fps);
-    input = new InputController(player, 100, 10);
+    player = new PlayerController(playerPos, playerSize, 1, 50, 1 / fps);
+    input = new InputController(player, 100, 100);
+}
+
+function setupLevels(){
+    let level0 = new Level();
+    level0.platforms.push(new Platform(createVector(300, 200), 50, 50));
+
+    levels.push(level0);
 }
 
 function draw() {
